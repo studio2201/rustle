d0c70e077e1e11f63c64ee7436909c1f44f5b30f
Jeryd <jeryd@users.noreply.github.com>
2026-06-23 21:41:33 -1000

    style: move stats button to left side next to info button in header

--- src/components/navbar.rs
+++ src/components/navbar.rs
@@ -91,6 +91,11 @@ pub fn navbar(props: &NavbarProps) -> Html {
                             <path stroke-linecap="round" stroke-linejoin="round" d="M9.879 7.519c1.171-1.025 3.071-1.025 4.242 0 1.172 1.025 1.172 2.687 0 3.712-.203.179-.43.326-.67.442-.745.361-1.45.999-1.45 1.827v.75M21 12a9 9 0 11-18 0 9 9 0 0118 0zm-9 5.25h.008v.008H12v-.008z" />
                         </svg>
                     </button>
+                    <button class="ml-3 focus:outline-none" onclick={move |_| stats_click.emit(())} aria-label="Stats">
+                        <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="h-6 w-6 cursor-pointer dark:stroke-white">
+                            <path stroke-linecap="round" stroke-linejoin="round" d="M3 13.125C3 12.504 3.504 12 4.125 12h2.25c.621 0 1.125.504 1.125 1.125v6.75C7.5 20.496 6.996 21 6.375 21h-2.25A1.125 1.125 0 013 19.875v-6.75zM9.75 8.625c0-.621.504-1.125 1.125-1.125h2.25c.621 0 1.125.504 1.125 1.125v11.25c0 .621-.504 1.125-1.125 1.125h-2.25a1.125 1.125 0 01-1.125-1.125V8.625zM16.5 4.125c0-.621.504-1.125 1.125-1.125h2.25C20.496 3 21 3.504 21 4.125v15.75c0 .621-.504 1.125-1.125 1.125h-2.25a1.125 1.125 0 01-1.125-1.125V4.125z" />
+                        </svg>
+                    </button>
                     {if ENABLE_ARCHIVED_GAMES {
                         html! {
                             <button class="ml-3 focus:outline-none" onclick={move |_| date_click.emit(())} aria-label="DatePicker">
@@ -105,11 +110,6 @@ pub fn navbar(props: &NavbarProps) -> Html {
                 </div>
                 <p class="absolute left-1/2 -translate-x-1/2 text-xl font-bold dark:text-white">{GAME_TITLE}</p>
                 <div class="right-icons">
-                    <button class="mr-3 focus:outline-none" onclick={move |_| stats_click.emit(())} aria-label="Stats">
-                        <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="h-6 w-6 cursor-pointer dark:stroke-white">
-                            <path stroke-linecap="round" stroke-linejoin="round" d="M3 13.125C3 12.504 3.504 12 4.125 12h2.25c.621 0 1.125.504 1.125 1.125v6.75C7.5 20.496 6.996 21 6.375 21h-2.25A1.125 1.125 0 013 19.875v-6.75zM9.75 8.625c0-.621.504-1.125 1.125-1.125h2.25c.621 0 1.125.504 1.125 1.125v11.25c0 .621-.504 1.125-1.125 1.125h-2.25a1.125 1.125 0 01-1.125-1.125V8.625zM16.5 4.125c0-.621.504-1.125 1.125-1.125h2.25C20.496 3 21 3.504 21 4.125v15.75c0 .621-.504 1.125-1.125 1.125h-2.25a1.125 1.125 0 01-1.125-1.125V4.125z" />
-                        </svg>
-                    </button>
                     <button class="mr-3 focus:outline-none" onclick={move |_| theme_click.emit(())} aria-label="Toggle theme">
                         {theme_toggle_icon}
                     </button>
